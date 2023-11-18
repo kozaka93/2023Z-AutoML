7355be5a5bf84a0dded068695bf000c9462750d7
galkowskim <mikolaj.galkowski7@gmail.com>
2023-11-18 15:04:15 +0100

    reformat script

--- homeworks/homework1/Bujakowski_Galkowski/search_best_default_hyperparameters.py
+++ homeworks/homework1/Bujakowski_Galkowski/search_best_default_hyperparameters.py
@@ -1,37 +1,38 @@
-import pickle
 import argparse
-import openml
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
-from xgboost import XGBClassifier
-from tqdm import tqdm
+import pickle
 import warnings
 
-warnings.filterwarnings('ignore')
+import numpy as np
+import openml
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+from xgboost import XGBClassifier
+
+warnings.filterwarnings("ignore")
 
 HYPERPARAMETERS_SPACE_RFC = {
-    'n_estimators': list(range(1, 2001)),
-    'max_depth': [None] + list(range(10, 51)),
-    'min_samples_split': list(range(2, 11)),
-    'min_samples_leaf': list(range(1, 5)),
-    'max_features': ['auto', 'sqrt', 'log2'] + list(np.arange(0.1, 1.1, 0.1)),
-    'bootstrap': [True, False],
-    'criterion': ['gini', 'entropy'],
-    'class_weight': [None, 'balanced'],
-    'max_samples': [None] + list(np.arange(0.1, 1.1, 0.1)),
+    "n_estimators": list(range(1, 2001)),
+    "max_depth": [None] + list(range(10, 51)),
+    "min_samples_split": list(range(2, 11)),
+    "min_samples_leaf": list(range(1, 5)),
+    "max_features": ["auto", "sqrt", "log2"] + list(np.arange(0.1, 1.1, 0.1)),
+    "bootstrap": [True, False],
+    "criterion": ["gini", "entropy"],
+    "class_weight": [None, "balanced"],
+    "max_samples": [None] + list(np.arange(0.1, 1.1, 0.1)),
 }
 
 HYPERPARAMETERS_SPACE_XGB = {
     "n_estimators": np.arange(1, 1000),
     "max_depth": np.arange(1, 1000),
     "max_leaves": np.arange(0, 10000),
-    'min_child_weight':np.arange(1, 50, 1),
-    "grow_policy": ['depthwise', 'lossguide'],
+    "min_child_weight": np.arange(1, 50, 1),
+    "grow_policy": ["depthwise", "lossguide"],
     "learning_rate": np.random.uniform(0, 1, 100),
-    "booster": ['gbtree', 'gblinear', 'dart'],
+    "booster": ["gbtree", "gblinear", "dart"],
     "gamma": np.random.uniform(0, 1, 100),
     "subsumple": np.random.uniform(0, 1, 100),
     "colsample_bytree": np.random.uniform(0, 1, 100),
@@ -40,20 +41,21 @@ HYPERPARAMETERS_SPACE_XGB = {
 }
 
 HYPERPARAMETERS_SPACE_LR = {
-    'penalty': ['l1', 'l2', 'elasticnet', None],
-    'l1_ratio': np.random.uniform(0, 1, 100),
-    'C': np.random.uniform(0, 250, 500),
-    'fit_intercept': [True],
+    "penalty": ["l1", "l2", "elasticnet", "none"],
+    "l1_ratio": np.random.uniform(0, 1, 100),
+    "C": np.random.uniform(0, 250, 500),
+    "fit_intercept": [True],
 }
 
-NO_ITER = 250
+NO_ITER = 500
+
 
 def randomly_choose_hyperparameters(model):
-    if model == 'RFC':
+    if model == "RFC":
         hyperparameters_space = HYPERPARAMETERS_SPACE_RFC
-    elif model == 'LR':
+    elif model == "LR":
         hyperparameters_space = HYPERPARAMETERS_SPACE_LR
-    elif model == 'XGB':
+    elif model == "XGB":
         hyperparameters_space = HYPERPARAMETERS_SPACE_XGB
 
     hyperparameters = {}
@@ -61,19 +63,15 @@ def randomly_choose_hyperparameters(model):
         hyperparameters[key] = np.random.choice(value)
     return hyperparameters
 
-labels = {
-    44: 'class',
-    1504: 'Class',
-    37: 'class',
-    1494: 'Class'
-}
+
+labels = {44: "class", 1504: "Class", 37: "class", 1494: "Class"}
 
 
 def main(args):
     model = args.model
 
     hparams_results = {}
-    for epoch in tqdm(range(NO_ITER)):
+    for _ in tqdm(range(NO_ITER)):
         while True:
             try:
                 chosen_hyperparameters = randomly_choose_hyperparameters(model)
@@ -84,21 +82,25 @@ def main(args):
                     dataset = openml.datasets.get_dataset(dataset_number)
                     df = dataset.get_data()[0]
                     df[label] = np.where(df[label] == df[label].cat.categories[0], 0, 1)
-                    train, test = train_test_split(df, test_size=0.2, random_state=42, stratify=df[label])
+                    train, test = train_test_split(
+                        df, test_size=0.2, random_state=42, stratify=df[label]
+                    )
 
                     X_train = train.drop(label, axis=1)
                     y_train = train[label]
                     X_test = test.drop(label, axis=1)
                     y_test = test[label]
 
-                    if model == 'RFC':
+                    if model == "RFC":
                         clf = RandomForestClassifier(**chosen_hyperparameters)
-                    elif model == 'LR':
-                        clf = LinearRegression(**chosen_hyperparameters)
-                    elif model == 'XGB':
-                        clf = XGBClassifier(**chosen_hyperparameters, enable_categorical=True)
+                    elif model == "LR":
+                        clf = LogisticRegression(**chosen_hyperparameters)
+                    elif model == "XGB":
+                        clf = XGBClassifier(
+                            **chosen_hyperparameters, enable_categorical=True
+                        )
                     else:
-                        raise ValueError('Model not supported')
+                        raise ValueError("Model not supported")
 
                     clf.fit(X_train, y_train)
 
@@ -113,7 +115,7 @@ def main(args):
                 else:
                     hparams_results[avg_auc] = [chosen_hyperparameters]
             except:
-                print('Error occured')
+                print("Error occured")
                 continue
             else:
                 break
@@ -126,11 +128,17 @@ def main(args):
     print(best_avg_auc)
     print(best_hparams_auc)
 
-    with open(f'best_hparams_auc_{model}.pickle', 'wb') as handle:
+    with open(f"best_hparams_auc_{model}.pickle", "wb") as handle:
         pickle.dump(best_hparams_auc, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Search best default hyperparameters')
-    parser.add_argument('--model', type=str, default='RFC', help='Model to search best default hyperparameters for')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Search best default hyperparameters")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="XGB",
+        help="Model to search best default hyperparameters for",
+    )
     args = parser.parse_args()
     main(args)
