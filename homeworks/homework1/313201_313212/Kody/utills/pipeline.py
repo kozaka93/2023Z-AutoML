58b9619c0f12ec3bb755c51313e067a1995049bc
Mieszko <mirgosmieszko@gmail.com>
2023-11-21 19:51:01 +0100

    change2

---
+++ homeworks/homework1/313201_313212/Kody/utills/pipeline.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+
+def get_column_transformer() -> ColumnTransformer:
+    num_pipeline = Pipeline(
+        steps=[("impute", SimpleImputer(strategy="mean")), ("scale", MinMaxScaler())]
+    )
+    cat_pipeline = Pipeline(
+        steps=[
+            ("impute", SimpleImputer(strategy="most_frequent")),
+            ("one-hot", OneHotEncoder(handle_unknown="ignore", sparse=False)),
+        ]
+    )
+
+    col_trans = ColumnTransformer(
+        transformers=[
+            (
+                "num_pipeline",
+                num_pipeline,
+                make_column_selector(dtype_include=np.number),
+            ),
+            ("cat_pipeline", cat_pipeline, make_column_selector(dtype_include=object)),
+        ],
+        remainder="drop",
+        n_jobs=-1,
+    )
+    return col_trans
+
+
+def evaluate_model(model: Pipeline, X_train, y_train, X_test, y_test) -> float:
+    model.fit(
+        X=X_train,
+        y=y_train,
+    )
+    return model.score(
+        X=X_test,
+        y=y_test,
+    )
